d15245bcee58d8f2167357c8dd3d18f0081b81c8
Nicolas <nicolasmartin89@gmail.com>
2025-01-08 10:52:27 -0300

    Setting up the Project

---
+++ src/main/java/com/nicom/shortenerurl/models/User.java
@@ -0,0 +1,19 @@
+package com.nicom.shortenerurl.models;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role = "ROLE_USER";
+
+}
